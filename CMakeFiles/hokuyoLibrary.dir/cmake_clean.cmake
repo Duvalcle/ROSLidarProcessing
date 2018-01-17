809436cdce902487b4ac2d57f4e2bb90ab7adac4
Duvalcle <duv.cle@gmai.com>
2018-01-17 19:19:40 +0100

    update serial.c attempt2 to untrack Cmake files

---
+++ CMakeFiles/hokuyoLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hokuyoLibrary.dir/src/Point.cpp.o"
+  "CMakeFiles/hokuyoLibrary.dir/src/Cluster.cpp.o"
+  "/home/kyrie/catkin_ws/devel/lib/libhokuyoLibrary.pdb"
+  "/home/kyrie/catkin_ws/devel/lib/libhokuyoLibrary.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hokuyoLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
